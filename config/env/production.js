cee8b6d16b7587109900ad8ee34fe2589e6d2517
maxamend <maxamend@espol.edu.ec>
2018-08-17 11:27:06 -0500

    [FIX] Configuraciones de permisos

--- config/env/production.js
+++ config/env/production.js
@@ -71,8 +71,8 @@ module.exports = {
       *                                                                           *
       ****************************************************************************/
       // ssl: true,
-      adapter:require('sails-postgresql'),
-      url: 'postgres://ubayzrpz:MT-EGdHBNyJe-ytl04SvYAr0-FGcTm0f@elmer.db.elephantsql.com:5432/ubayzrpz'
+      //adapter:require('sails-postgresql'),
+      url: 'postgres://hwtciktbuwvtmg:93aeee3a8f36ea56f4d54ba42266edeaa60b71be5b48d6b264efb32e5f0166dc@ec2-50-17-250-38.compute-1.amazonaws.com:5432/d98uk5dj1sifu'
 
     //},
     //connection: 'postgres'
