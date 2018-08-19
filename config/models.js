2f2606ab1f2e139a7c78ca608c831a8a8f7ff955
maxamend <maxamend@espol.edu.ec>
2018-08-19 03:37:07 -0500

    [FIX] config para configurar la base a heroku

--- config/models.js
+++ config/models.js
@@ -13,6 +13,19 @@
  * https://sailsjs.com/docs/concepts/models-and-orm/model-settings
  */
 
+ function generatePostgresqlObject() {
+  const local = String(process.env.DATABASE_URL).indexOf('localhost') !== -1
+
+  const adapter = 'sails-postgresql';
+  const url = local ? undefined : process.env.DATABASE_URL;
+  const address = local ? process.env.DATABASE_URL : undefined;
+  const ssl = !local;
+  const database = local ? 'sails_todo' : undefined;
+
+  const pgObject = { adapter, url, address, ssl, database };
+  return pgObject;
+}
+
 module.exports.models = {
 
 
@@ -120,8 +133,9 @@ module.exports.models = {
 
   cascadeOnDestroy: true,
 
-
+  somePostgresqlServer: generatePostgresqlObject(),
   connection: 'somePostgresqlServer',
   migrate: 'safe',
 
 };
+
